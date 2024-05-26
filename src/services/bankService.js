4af0fdd437c190cbb0c2c34390eaed108218501d
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 22:52:13 -0600

    Depuración adicional

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -1,12 +1,16 @@
 const BANKS_URL = "https://cors-anywhere.herokuapp.com/https://dev.obtenmas.com/catom/api/challenge/banks";
 
 export const fetchBanks = async () => {
-  const storedBanks = localStorage.getItem('banks');
-  if (storedBanks) return JSON.parse(storedBanks);
-
-  const response = await fetch(BANKS_URL);
-  const data = await response.json();
-  console.log('data: ', data);
-  localStorage.setItem('banks', JSON.stringify(data));
-  return data; 
-}
+    const storedBanks = localStorage.getItem('banks');
+    if (storedBanks && storedBanks !== "") {
+      console.log("Using stored data:", storedBanks);
+      return JSON.parse(storedBanks);
+    }
+  
+    const response = await fetch(BANKS_URL);
+    const data = await response.json();
+    console.log('data from API:', data);
+    localStorage.setItem('banks', JSON.stringify(data));
+    return data; 
+  }
+  
