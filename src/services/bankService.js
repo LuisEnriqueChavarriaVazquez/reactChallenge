e66b1cc26483c498875aab89ddc34fef3eb53bce
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 23:40:14 -0600

    Inclusion de API proxy en la URL de consulta

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -10,7 +10,7 @@ export const fetchBanks = async () => {
     const response = await fetch(BANKS_URL);
     const data = await response.json();
     console.log('data from API:', data);
-    localStorage.setItem('banks', JSON.stringify(data));
+    localStorage.setItem('banks', JSON.stringify(data)); 
     return data;  
   }
   
