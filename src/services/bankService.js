baeca17da1daae59b626a14fe0ab1bde83dc6ae5
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 22:45:56 -0600

    Prueba de JSON Stringify

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -2,11 +2,11 @@ const BANKS_URL = "https://cors-anywhere.herokuapp.com/https://dev.obtenmas.com/
 
 export const fetchBanks = async () => {
   const storedBanks = localStorage.getItem('banks');
-  console.log('storedBanks: ', storedBanks);
   if (storedBanks) return JSON.parse(storedBanks);
 
   const response = await fetch(BANKS_URL);
   const data = await response.json();
+  console.log('data: ', data);
   localStorage.setItem('banks', JSON.stringify(data));
   return data; 
 }
