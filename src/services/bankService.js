03b7020adac1100e2a92b1f7401e1ff4b3ea4931
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 23:33:34 -0600

    Server proxy en funcionamiento

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -1,4 +1,4 @@
-const BANKS_URL = "https://cors-anywhere.herokuapp.com/https://dev.obtenmas.com/catom/api/challenge/banks";
+const BANKS_URL = "https://react-challenge-server-dr42v1l3c-cuenta-movils-projects.vercel.app/";
 
 export const fetchBanks = async () => {
     const storedBanks = localStorage.getItem('banks');
