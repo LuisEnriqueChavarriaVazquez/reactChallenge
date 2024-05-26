7d4444217549860c8e7d85f576f02a847362193a
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-26 06:19:50 -0600

    Update del fetch cada 24 horas para garantizar que los datos son actuales

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -1,16 +1,28 @@
-const BANKS_URL = "https://react-challenge-server-theta.vercel.app/";
+//const BANKS_URL = "https://react-challenge-server-theta.vercel.app/";
+const BANKS_URL = "https://dev.obtenmas.com/catom/api/challenge/banks";
 
 export const fetchBanks = async () => {
-    const storedBanks = localStorage.getItem('banks');
-    if (storedBanks && storedBanks !== "") {
-      console.log("Using stored data:", storedBanks);
-      return JSON.parse(storedBanks);
-    }
+    try {
+      const storedBanks = localStorage.getItem('banks');
+      const lastFetch = localStorage.getItem('lastFetch');
+      const now = new Date();
   
-    const response = await fetch(BANKS_URL);
-    const data = await response.json();
-    console.log('data from API:', data);
-    localStorage.setItem('banks', JSON.stringify(data)); 
-    return data;  
-  }
+      // Verifica si los datos están en localStorage y no han pasado más de 24 horas
+      if (storedBanks && lastFetch && (now - new Date(lastFetch)) < 86400000) {
+        console.log("Using stored data:", storedBanks);
+        return JSON.parse(storedBanks);
+      }
+  
+      // Fetch new data if not found in localStorage or data is older than 24 hours
+      const response = await fetch(BANKS_URL);
+      const data = await response.json();
+      console.log('Data from API:', data);
+      localStorage.setItem('banks', JSON.stringify(data));
+      localStorage.setItem('lastFetch', new Date().toISOString());
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch banks:", error);
+      throw error;  // Re-throw the error if you want calling function to handle it
+    }
+  };
   
