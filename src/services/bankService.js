6ee9cbfa409ab0988a5c0f4cb563d0420c3f2735
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 22:53:28 -0600

    Prueba de JSON Stringify

--- src/services/bankService.js
+++ src/services/bankService.js
@@ -11,6 +11,6 @@ export const fetchBanks = async () => {
     const data = await response.json();
     console.log('data from API:', data);
     localStorage.setItem('banks', JSON.stringify(data));
-    return data; 
+    return data;  
   }
   
